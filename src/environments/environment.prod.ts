81b87a8ab844aab8a6df4663c23548ceee95da4f
cetinmetin <metincetin1996@gmail.com>
2021-05-04 16:01:35 +0300

    Changes Have Been Made for Deploy to Firebase Hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
-  production: true,
-  apiBaseUrl: '',
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDMorrrBPg-gQzvYVS6dFxMAYXv-tSov-Y",
+    authDomain: "denetciapp.firebaseapp.com",
+    databaseURL: "https://denetciapp-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "denetciapp",
+    storageBucket: "denetciapp.appspot.com",
+    messagingSenderId: "292430766669",
+    appId: "1:292430766669:web:58e7d3d150bb20a6b2f204",
+    measurementId: "G-RKXNY2PELR"
+},
 };
